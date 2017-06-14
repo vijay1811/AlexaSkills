6b09c5bb7f53d6d4fc4e27472d4bd032db7c7675
Paritosh Agrawal <paritosh.agrawal@nagarro.com>
2017-06-14 18:49:55 +0530

    Final Push for Conversational API

--- requesthandlers/alexa/application/buildhelper/handler_request_intent.go
+++ requesthandlers/alexa/application/buildhelper/handler_request_intent.go
@@ -50,21 +50,10 @@ func getOutputSpeech(intent *alexa.Intent, attributes map[string]*alexa.Slot) (*
 	var outSpeech *alexa.OutputSpeech
 	var isComplete bool
 
-	// // starts with all the slots from the attributes
-	// slots := attributes
-
-	// // inputs are the new slot values from new intent
-	// for slotName, slot := range intent.Slots {
-	// 	if slot.Value != "" {
-	// 		slots[slotName] = slot
-	// 	}
-	// }
-
 	// All the newly requested slots in the intent must be saved in a new map.
 	slots := intent.Slots
 
 	for slotName := range attributes {
-		// log.Printf("SLOTNAME: %s, SLOT: ", ...)
 		intentSlot := slots[slotName]
 		log.Printf("Intent Slot: %+v", intentSlot)
 
@@ -75,28 +64,7 @@ func getOutputSpeech(intent *alexa.Intent, attributes map[string]*alexa.Slot) (*
 			delete(slots, slotName)
 			slots[slotName] = attributeSlot
 		}
-
-		// if intentSlot.Value != "" {
-		// 	if attributeSlot.Value != "" {
-		// 		intentSlot.Value = attributeSlot.Value
-		// 	}
-		// }
-
-		// Looks for the value in the slots for the key 'slotName' (for updated values)
-		// if slots[slotName].Value != "" {
-		// 	// Searches for value in the attributes for the key 'slotName' as backup
-		// 	if attributes[slotName].Value != "" {
-		// 		// if found, copy the value from attributes to slots
-		// 		slots[slotName].Value = attributes[slotName].Value
-		// 	}
-		// }
 	}
-
-	// for key := range attributes {
-	// 	if _, exists := slots[key]; !exists {
-	// 		slots[key] = attributes[key]
-	// 	}
-	// }
 
 	buildTypeSlot := slots["buildtypeslot"]
 	buildSourceTypeSlot := slots["buildsourcetypeslot"]
@@ -113,9 +81,6 @@ func getOutputSpeech(intent *alexa.Intent, attributes map[string]*alexa.Slot) (*
 		sourceType = buildSourceTypeSlot.Value
 		sourceTypeGiven = true
 	}
-
-	// buildType, buildTypeGiven := slots["buildtypeslot"]
-	// sourceType, sourceTypeGiven := slots["buildsourcetypeslot"]
 
 	switch {
 	case buildTypeGiven && sourceTypeGiven:
@@ -148,6 +113,3 @@ func getOutputSpeech(intent *alexa.Intent, attributes map[string]*alexa.Slot) (*
 
 	return outSpeech, slots, isComplete
 }
-
-/*amzn1.ask.skill.462a6bc5-8525-48cd-9b6a-bc862fc1b667
-amzn1.ask.skill.462a6bc5-8525-48cd-9b6a-bc862fc1b667*/
