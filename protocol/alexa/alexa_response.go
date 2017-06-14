99ce62e1ea33b1a9fa1a2a9739d832901832fc3c
Paritosh Agrawal <paritosh.agrawal@nagarro.com>
2017-06-14 13:33:33 +0530

    Fixing JSON Marshal error

--- protocol/alexa/alexa_response.go
+++ protocol/alexa/alexa_response.go
@@ -2,7 +2,7 @@ package alexa
 
 type AlexaResponse struct {
 	Version           string           `json:"version"`
-	SessionAttributes map[string]*Slot `json:"sessionattributes,omitempty"`
+	SessionAttributes map[string]*Slot `json:"sessionAttributes,omitempty"`
 	Response          *Response        `json:"response"`
 }
 
